52392534836e857c56da3eb25aedfe101910e4aa
Jakub Pocentek <jakub.pocentek@apertogroup.com>
2014-04-23 18:04:58 +0200

    Begin work with user panel

--- userspace/models.py
+++ userspace/models.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.ImageField(
+        upload_to='/img/avatars', 
+        default='/img/avatars/anonymous.png'
+    )
