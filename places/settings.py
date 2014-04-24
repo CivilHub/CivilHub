b41955690d4320a4797cb8a86f23cbbf0864a935
Krastley <krastley@gmail.com>
2014-04-24 19:58:09 +0200

    All paths changed to relative

--- places/settings.py
+++ places/settings.py
@@ -115,8 +115,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-#MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
-MEDIA_ROOT = '/home/chester/Dev/django/places/media/'
-MEDIA_URL = '/media/'
+STATIC_URL   = '/static/'
+MEDIA_ROOT   = os.path.join(BASE_DIR, 'media')
+MEDIA_URL    = '/media/'
