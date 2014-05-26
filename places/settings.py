3a6807db23ee9d228970be9b4b08050322c5ebbf
Jakub Pocentek <jakub.pocentek@apertogroup.com>
2014-05-26 11:22:59 +0200

    Tłumaczenia w szablonach

--- places/settings.py
+++ places/settings.py
@@ -167,6 +167,11 @@ USE_L10N = True
 USE_TZ = True
 
 
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
