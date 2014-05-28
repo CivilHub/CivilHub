f9038ff2fe3a6c22a33ea85f203336f85036abfd
Jakub Pocentek <jakub.pocentek@apertogroup.com>
2014-05-28 13:36:41 +0200

    Jakieś zmiany

--- places/settings.py
+++ places/settings.py
@@ -162,8 +162,14 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ATOMIC_REQUESTS': False,
     }
+    #~ 'default': {
+        #~ 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #~ 'NAME': 'places',                      
+        #~ 'USER': 'places',
+        #~ 'PASSWORD': '233mmx',
+        #~ 'HOST': ''
+    #~ }
 }
 
 # Internationalization
