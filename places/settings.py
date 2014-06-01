a69b6cb10e035b65a8af0cbe30f534c482595d8d
Krastaman Rastamarian <chester@Expose4.expose>
2014-06-01 09:40:12 +0200

    Developer switched back to sqlite

--- places/settings.py
+++ places/settings.py
@@ -166,17 +166,23 @@ MESSAGE_TAGS = {
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
+#DATABASES = {
     #~ 'default': {
         #~ 'ENGINE': 'django.db.backends.sqlite3',
         #~ 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     #~ }
+    #~ 'default': {
+        #~ 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #~ 'NAME': 'places',                      
+        #~ 'USER': 'places',
+        #~ 'PASSWORD': '233mmx',
+        #~ 'HOST': ''
+    #~ }
+#~ }
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'places',                      
-        'USER': 'places',
-        'PASSWORD': '233mmx',
-        'HOST': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
