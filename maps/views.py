2b208905103bd6e3ab15943e7fa397f98c865752
Krastaman Rastamarian <chester@debian.expose>
2014-06-02 16:26:48 +0200

    Fixed bug with map content types

--- maps/views.py
+++ maps/views.py
@@ -42,6 +42,7 @@ def get_pointers(request):
                 'longitude': l.longitude,
                 'name'     : l.name,
                 'url'      : l.get_absolute_url(),
+                'type'     : str(ContentType.objects.get_for_model(l)),
             })
     for p in ps:
         pointers.append({
@@ -50,6 +51,7 @@ def get_pointers(request):
             'content_type': p.content_type.pk,
             'object_pk'   : p.object_pk,
             'url'         : p.content_object.get_absolute_url(),
+            'type'        : str(p.content_type),
         })
     context = {
         'success'  : True,
