76bb011a6c4fa5a2b52096bb3531e21368ca133a
Jakub Pocentek <jpocentek@gmail.com>
2014-06-17 17:16:45 +0200

    Test

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class LocationGalleryItem(models.Model):
+    """
+    This is basic class for items in location's gallery. File upload is handled
+    other way, here we just add some additional data for our pictures, like
+    comment and name.
+    """
+    name = models.CharField(max_length=64, blank=True, null=True)
+    user = models.ForeignKey(User)
+    date_uploaded = models.DateTimeField(auto_now_add=True)
+    picture_name  = models.CharField(max_length=256)
+    description   = models.TextField(blank=True, null=True)
+
+    def get_thumbnail(self, size):
+        """
+        This function returns url to thumbnail of photo. Size parameter is man-
+        datory and related to thumb sizes declared in views module. It should
+        be touple or list containing images width and height.
+        """
+        return str(size[0]) + 'x' + str(size[1]) + self.picture_name
+
+    def __unicode__(self):
+        return self.name
