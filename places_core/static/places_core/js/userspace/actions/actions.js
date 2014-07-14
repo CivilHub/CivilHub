6af5ea78d11c862ff16ba36436a42ea54856e3df
Jakub Pocentek <jpocentek@gmail.com>
2014-07-14 10:00:25 +0200

    Actions finished

--- places_core/static/places_core/js/userspace/actions/actions.js
+++ places_core/static/places_core/js/userspace/actions/actions.js
@@ -12,6 +12,12 @@ function ($, ActionList) {
     
     var actions = new ActionList();
     
+    // Check if there is a better way to handle external events.
+    $('.list-controller').on('click', function (e) {
+        e.preventDefault();
+        actions.filter($(this).attr('data-target'));
+    });
+    
     // Enable lazy-loading on page scrolling
     $(window).scroll(function() {
        if($(window).scrollTop() + $(window).height() == $(document).height()) {
