4edc81702a58de31c5da87ad7698fdf9a2b30b99
Jakub Pocentek <jpocentek@gmail.com>
2014-10-02 09:57:09 +0200

    Fixed homepage google scripts

--- places_core/static/places_core/js/build/staticpages/home/home-main.js
+++ places_core/static/places_core/js/build/staticpages/home/home-main.js
@@ -41,10 +41,45 @@ require.config({
     }
 });
 
+//
+// Logowanie przez Google+
+// -----------------------------------------------------------------------------
+
+var signInCallback = function (result) {
+    if (result['error']) {
+        console.log('An error happened:', result['error']);
+    } else {
+        $('#code').attr('value', result['code']);
+        $('#at').attr('value', result['access_token']);
+        $('#google-plus').submit();
+    }
+};
+
+/* Executed when the APIs finish loading */
+function render() {
+    // Additional params including the callback, the rest of the params will
+    // come from the page-level configuration.
+    var additionalParams = {
+        'scope': window.GOOGLE_DATA.scope,
+        'clientid': window.GOOGLE_DATA.id,
+        'accesstype': 'offline',
+        'cookiepolicy': 'single_host_origin',
+        'callback': 'signInCallback'
+    };
+
+    // Attach a click listener to a button to trigger the flow.
+    var signinButton = document.getElementById('googleplus');
+    signinButton.addEventListener('click', function() {
+        gapi.auth.signIn(additionalParams); // Will use page level configuration
+    });
+}
+
 require(['jquery',
          'bootstrap',
          'js/common',
-         'js/ui/validate'],
+         'js/ui/validate',
+         'async!https://apis.google.com/js/plusone.js',
+         'async!https://plus.google.com/js/client:plusone.js?onload=render'],
 
 function ($) {
     
