63c2913bb0c30f5f0e037575562786a932924b1e
Jakub Pocentek <jpocentek@gmail.com>
2014-10-02 09:24:07 +0200

    Updated 'home' template

--- places/settings.py
+++ places/settings.py
@@ -154,6 +154,7 @@ DATABASES = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+
 STATIC_URL   = '/static/'
 MEDIA_ROOT   = os.path.join(BASE_DIR, 'media')
 MEDIA_URL    = '/media/'
