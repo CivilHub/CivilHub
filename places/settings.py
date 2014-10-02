be8300d2ca27763c7837eb6f0113c8478727d3c6
Jakub Pocentek <jpocentek@gmail.com>
2014-10-02 10:24:33 +0200

    Fixed home template

--- places/settings.py
+++ places/settings.py
@@ -321,6 +321,19 @@ LANGUAGES = (
 )
 
 
+# REDIS CACHE
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '127.0.0.1:6379:1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+        }
+    }
+}
+
+
 # Email settings
 #-------------------------------------------------------------------------------
 # Email account settings
