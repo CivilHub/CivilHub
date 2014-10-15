347d7ed0edb469c0df784d25342f3f63a01daab6
Jakub Pocentek <jpocentek@gmail.com>
2014-10-15 14:35:18 +0200

    Help and support

--- articles/models.py
+++ articles/models.py
@@ -5,7 +5,7 @@ from mptt.models import MPTTModel, TreeForeignKey
 
 
 class Category(MPTTModel):
-    """ """
+    """ Nested category model. """
     name = models.CharField(max_length=64)
     description = models.TextField(null=True, blank=True)
     parent = TreeForeignKey('self', null=True, blank=True,
@@ -19,7 +19,7 @@ class Category(MPTTModel):
 
 
 class Article(models.Model):
-    """ """
+    """ Basic model for different types of articles. """
     title = models.CharField(max_length=64)
     slug = models.SlugField(max_length=64)
     content = models.TextField()
@@ -28,9 +28,24 @@ class Article(models.Model):
     category = models.ForeignKey(Category, blank=True, null=True)
 
     def get_absolute_url(self):
+        """ 
+        We have to treat support entries differently then others. It's possible
+        to add support for nested categories and link them to urls if this will
+        be desired.
+        """
         if self.category:
-            return reverse('articles:'+self.category.name_en.lower()+'_entry',
-                            kwargs={'slug':self.slug})
+            try:
+                c = Category.objects.get(name__icontains=u'support')
+            except Category.DoesNotExist:
+                c = None
+            # Category exists and article is descendant
+            if c is not None and self.category.is_descendant_of(c):
+                urlentry = 'articles:support_entry'
+            # Article belongs to other category, not nested to support tree
+            else:
+                urlentry = 'articles:{}_entry'.format(self.category.name)
+            return reverse(urlentry, kwargs={'slug':self.slug})
+        # stand-alone article - remember to configure urls properly!!!
         return '/' + self.slug + '/'
 
     def __unicode__(self):
--- articles/views.py
+++ articles/views.py
@@ -1,19 +1,103 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.contrib.contenttypes.models import ContentType
-from .models import Article
+from django.utils.translation import ugettext as _
+from .models import Category, Article
 
+# Helpers
+#-------------------------------------------------------------------------------
+
+def expand_category_tree(category):
+    """
+    This function returns dictionary of elements containing all subcategories of
+    selected category passed as argument and links related to articles belonging
+    to these categories. Returns dictionary content which could be included in
+    template. Function run recursively from top (provided category instance) 
+    to bottom (last article in tree).
+    """
+    links = {
+        'category': {
+            'name': category.name,
+            'level': category.level,
+        },
+        'subcategories': [],
+        'articles': [],
+    }
+    for sub in category.subcategories.all():
+        links['subcategories'].append(expand_category_tree(sub))
+    for art in category.article_set.all():
+        article_link = {
+            'title': art.title,
+            'url': art.get_absolute_url(),
+        }
+        links['articles'].append(article_link)
+    return links
+
+
+def print_category_tree(category):
+    """
+    This function returns HTML tree structure related to selected category and
+    divided into links for subcategories and for single articles.
+    """
+    art_tpl = '<li class="article-link"><a href="{}">{}</a></li>'
+    html = '<li class="category-entry"><a href="#">{}</a><ul class="sublist sub-lvl-{}">' \
+            .format(category.name, category.level)
+    for cat in category.subcategories.all():
+        html += print_category_tree(cat)
+    for article in category.article_set.all():
+        html += art_tpl.format(article.get_absolute_url(), article.title)
+    return html + '</ul></li>'
+
+
+def print_menu(category):
+    """
+    Function returns HTML content for entire menu for selected category. Made
+    to be used with Support section. This way we can omit parent category and
+    display just categories related to this one.
+    """
+    html = ''
+    for sub in category.subcategories.all():
+        html += print_category_tree(sub)
+    return html
+
+
+# Support
+#-------------------------------------------------------------------------------
+
+class SupportListView(TemplateView):
+    """ Show list of support and help topics. """
+    template_name = "articles/support.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(SupportListView, self).get_context_data(**kwargs)
+        context['title'] = _("Help and support")
+        try:
+            category = Category.objects.get(name__icontains=u'support')
+            context['menu'] = print_menu(category)
+        except Category.DoesNotExist:
+            context['menu'] = _("Support category does not exist yet")
+        return context
+
+
+class SupportEntryView(DetailView):
+    """ Detailed support entry about specific topic. """
+    model = Article
+    template_name = "articles/entry.html"
+
+
+# blog
+#-------------------------------------------------------------------------------    
 
 class BlogListView(ListView):
-    """ """
+    """ Manage list of all blog entries. """
     queryset = Article.objects.filter(category__name='Blog')
     context_object_name = "articles"
     template_name = "articles/blog.html"
 
 
 class BlogEntryView(DetailView):
-    """ """
+    """ Display single blog entry. """
     queryset = Article.objects.filter(category__name='Blog')
     template_name = "articles/article.html"
 
@@ -23,22 +107,12 @@ class BlogEntryView(DetailView):
         return context
 
 
-class SupportListView(ListView):
-    """ """
-    queryset = Article.objects.filter(category__name='Support')
-    context_object_name = "articles"
-    template_name = "articles/support.html"
-
-
-class SupportEntryView(DetailView):
-    """ """
-    queryset = Article.objects.filter(category__name='Support')
-    template_name = "articles/entry.html"
-
+# Stand-alone articles
+#-------------------------------------------------------------------------------
 
 class TopLevelArticleView(DetailView):
-    """ """
-    queryset = Article.objects.all()
+    """ Show any article in top-level url hierarchy (as static page). """
+    model = Article
     article_slug = None
     template_name = "articles/article.html"
 
