7d71499eff6134932de46ee664f804bcdf967b97
Jakub Pocentek <jpocentek@gmail.com>
2014-10-15 14:03:04 +0200

    Help and support tree menu

---
+++ places_core/static/places_core/js/build/articles/support/support-main.js
@@ -0,0 +1,102 @@
+
+// Accordion menu and article content loader for Help and Support page.
+
+require.config({
+    
+    baseUrl: window.STATIC_URL,
+    
+    urlArgs: "bust=" + (new Date()).getTime(),
+    
+    waitSeconds: 200,
+    
+    paths: {
+        jquery: 'includes/jquery/jquery',
+        bootstrap: 'includes/bootstrap/bootstrap',
+        underscore: 'includes/underscore/underscore',
+        backbone: 'includes/backbone/backbone',
+        tagsinput: 'includes/jquery/jquery.tagsinput',
+        bootbox: 'includes/bootstrap/bootbox'
+    },
+    
+    shim: {
+        underscore: {
+            deps: ['jquery'],
+            exports: '_'
+        },
+        
+        backbone: {
+            deps: ['underscore'],
+            exports: 'Backbone'
+        },
+        
+        bootstrap: {
+            deps: ['jquery']
+        },
+        
+        bootbox: {
+            deps: ['bootstrap'],
+            exports: 'bootbox'
+        },
+        
+        tagsinput: {
+            deps: ['jquery']
+        },
+    }
+});
+
+require(['jquery',
+         'js/common'],
+         
+function ($) {
+    
+    "use strict";
+    
+    $('#category-menu a').on('click', function (e) {
+        e.preventDefault();
+        $('#category-menu').find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+    
+    $('.category-entry > a').on('click', function (e) {
+        
+        e.preventDefault();
+        
+        var $sublist = $(this).next('.sublist');
+        
+        // Holds proper classname for first element to show (category/article)
+        
+        var current = null;
+        
+        $(this).parent().siblings().each(function () {
+            $(this)
+                .find('.sublist:first')
+                .not($sublist)
+                    .slideUp('fast')
+                .find('.sublist')
+                    .slideUp('fast');
+        });
+        
+        $sublist.slideToggle('fast', function () {
+            
+            // If there are articles, show first, otherwise expand first category.
+            if ($(this).children('.article-link').length >= 1) {
+                current = '.article-link:first';
+            } else {
+                current = '.category-entry:first'
+            }
+            $sublist.children(current).find('a:first').trigger('click');
+        });
+    });
+    
+    $('.article-link > a').on('click', function (e) {
+        e.preventDefault();
+        $.get($(this).attr('href'), function (content) {
+            $('#article-content').html(content);
+        });
+    });
+    
+    $('.category-entry:first').find('a').trigger('click');
+    
+    $(document).trigger('load');
+    
+});
