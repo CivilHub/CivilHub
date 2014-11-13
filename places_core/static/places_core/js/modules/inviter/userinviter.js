3e02fe4c6d948d43eb61d2201ec0fcf819b6afaf
Jakub Pocentek <jpocentek@gmail.com>
2014-11-13 14:35:29 +0100

    Postman pages look improved

--- places_core/static/places_core/js/modules/inviter/userinviter.js
+++ places_core/static/places_core/js/modules/inviter/userinviter.js
@@ -14,7 +14,7 @@ function ($) {
         $form      = $modal.find('form:first'),        // Form
         $submit    = $modal.find('.submit-btn'),       // Submit button
         $emails    = $('#invite-emails'),              // Emails input
-        inviteText = $emails.attr('data-defaulttext'), // Text for TagsInput
+        inviteText = '',                               // Text for TagsInput
         apiUrl     = $form.attr('action'),             // Api target URL
         currentUrl = window.location.href;             // Current page address
 
