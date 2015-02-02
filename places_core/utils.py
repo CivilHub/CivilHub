bde5ede341cbf28ef0a037dea8221aafbb81454f
Jakub Pocentek <jpocentek@gmail.com>
2015-02-02 13:30:58 +0100

    Skrypty do eksportowania zawartości

---
+++ places_core/utils.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import os, json, codecs
+
+from django.conf import settings
+
+DIR = os.path.join(settings.BASE_DIR, 'dump')
+
+
+def serialize_item(model):
+    """ Zamienia wartości pól modelu na zserializowane wartości. Przyjmuje
+    konkretną instancję modelu jako argument i zwraca słownik. """
+    model_dict = {}
+    for key, val in model.__dict__.iteritems():
+        if key.startswith('_'):
+            continue
+        elif type(model.__dict__[key]).__name__ == 'ImageFieldFile':
+            model_dict.update({key: model.__dict__[key].path})
+        elif type(model.__dict__[key]).__name__ == 'datetime':
+            model_dict.update({key: model.__dict__[key].isoformat()})
+        else:
+            model_dict.update({key: model.serializable_value(key)})
+    return model_dict
+
+
+def serialize_models(model_class):
+    """
+    Serializuje wszystkie obiekty z danego modelu i zwraca dane w formacie
+    json.
+    """
+    model_data = []
+    for item in model_class.objects.all():
+        model_data.append(serialize_item(item))
+    return json.dumps(model_data)
+
+
+def export_items(model_class):
+    """ Eksportuje wszystkie obiekty powiązane z danym modelem (klasę obiektów
+    należy przekazać jako parametr do funkcji) do uniwersalnej postaci JSON
+    i zapisuje wyniki do pliku w odpowiednim folderze. """
+    dirname = os.path.join(DIR, model_class.__module__.split('.')[0].lower())
+    if not os.path.isdir(dirname):
+        os.mkdir(dirname)
+    filename = os.path.join(dirname, model_class.__name__.lower() + '.json')
+    f = codecs.open(filename, 'w+', 'utf-8')
+    f.write(serialize_models(model_class))
+    f.close()
