2372343d7b845d34af03af4a945755c16d3b9ca1
Jakub Pocentek <jpocentek@gmail.com>
2015-02-25 13:39:57 +0100

    Poprawione zależności w skryptach formularzy

--- places_core/static/places_core/js/src/discussion-create.js
+++ places_core/static/places_core/js/src/discussion-create.js
@@ -13,67 +13,108 @@ require.config({
     
     waitSeconds: 200,
     
-    paths: {
-        jquery             : "includes/jquery/jquery",
+    "paths": {
+        "jquery": "includes/jquery/jquery",
         "jqueryui": "includes/jquery-ui/jquery-ui",
-        jpaginate          : "includes/jquery/jquery.paginate",
-        jqueryui           : "includes/jquery-ui/jquery-ui",
-        tagsinput          : "includes/jquery/jquery.tagsinput",
-        bootstrap          : "includes/bootstrap/bootstrap",
-        bootbox            : "includes/bootstrap/bootbox",
+        "jpaginate": "includes/jquery/jquery.paginate",
+        "bootstrap": "includes/bootstrap/bootstrap",
+        "bootbox": "includes/bootstrap/bootbox",
+        "underscore": "includes/underscore/underscore",
+        "backbone": "includes/backbone/backbone",
+        "paginator": "includes/backbone/backbone.paginator",
+        "moment": "includes/momentjs/moment",
+        "tagsinput": "includes/jquery/jquery.tagsinput",
         "bootstrap-switch" : "includes/bootstrap/bootstrap-switch",
-        underscore         : "includes/underscore/underscore",
-        backbone           : "includes/backbone/backbone",
-        redactor           : "includes/redactor/redactor",
-        dropzone           : "includes/dropzone/dropzone",
-        leaflet            : "includes/leaflet/leaflet",
-        tour               : "includes/tour/bootstrap-tour"
+        "redactor": "includes/redactor/redactor",
+        "dropzone": "includes/dropzone/dropzone",
+        "leaflet": "includes/leaflet/leaflet",
+        "fullpagejs": "includes/fullpagejs/jquery.fullPage",
+        "color": "includes/jquery/jquery.color",
+        "Jcrop": "includes/jquery/jquery.Jcrop",
+        "file-input": "includes/bootstrap/bootstrap.file-input",
+        "vector": "includes/vectormap/jquery-jvectormap-1.2.2.min",
+        "worldmap": "includes/vectormap/jquery-jvectormap-world-mill-en",
+        "plot": "includes/jqplot/jquery.jqplot",
+        "dateAxisRenderer": "includes/jqplot/plugins/jqplot.dateAxisRenderer",
+        "canvasTextRenderer": "includes/jqplot/plugins/jqplot.canvasTextRenderer",
+        "canvasAxisTickRenderer": "includes/jqplot/plugins/jqplot.canvasAxisTickRenderer",
+        "categoryAxisRenderer": "includes/jqplot/plugins/jqplot.categoryAxisRenderer",
+        "barRenderer": "includes/jqplot/plugins/jqplot.barRenderer",
+        "tubular": "includes/tubular/jquery.tubular.1.0",
+        "tour": "includes/tour/bootstrap-tour"
     },
-    
-    shim: {
+
+    "shim": {
         
-        jpaginate: {
-            deps: ["jquery"]
+        "jpaginate": {
+            "deps": ["jquery"]
         },
         
-        jqueryui: {
-            deps: ["jquery"]
+        "underscore": {
+            "deps": ["jquery"],
+            "exports": "_"
         },
         
-        tagsinput: {
-            deps: ["jqueryui"]
+        "backbone": {
+            "deps": ["underscore"],
+            "exports": "Backbone"
         },
         
-        underscore: {
-            deps: ["jquery"],
-            exports: "_"
+        "bootstrap": {
+            "deps": ["jquery"]
         },
         
-        backbone: {
-            deps: ["underscore"],
-            exports: "Backbone"
+        "bootbox": {
+            "deps": ["bootstrap"],
+            "exports": "bootbox"
         },
         
-        bootstrap: {
-            deps: ["jquery"]
+        "tagsinput": {
+            "deps": ["jquery"]
         },
-        
-        bootbox: {
-            deps: ["bootstrap"],
-            exports: "bootbox"
-        },
-        
+
         "bootstrap-switch": {
-            deps: ["bootstrap"]
+            "deps": ["bootstrap"]
+        },
+
+        "fullpagejs": {
+            "deps": ["jquery"]
         },
         
-        mapinput: {
-            deps: ["jquery"]
-        }
+        "color": {
+            "deps": ["jquery"]
+        },
+        
+        "Jcrop": {
+            "deps": ["color"]
+        },
+        
+        "file-input": {
+            "deps": ["bootstrap"]
+        },
+        
+        "vector": {
+            "deps": ["jquery"]
+        },
+        
+        "worldmap": {
+            "deps": ["vector"]
+        },
+        
+        "plot": {
+            "deps": ["jquery"]
+        },
+        
+        "dateAxisRenderer": { "deps": ["plot"] },
+        "canvasTextRenderer": { "deps": ["plot"] },
+        "canvasAxisTickRenderer": { "deps": ["plot"] },
+        "categoryAxisRenderer": { "deps": ["plot"] },
+        "barRenderer": { "deps": ["plot"] }
     }
 });
 
 require(['jquery',
+         'jqueryui',
          'js/modules/common',
          'js/modules/locations/follow',
          'js/modules/editor/plugins/uploader',
--- places_core/static/places_core/js/src/news-create.js
+++ places_core/static/places_core/js/src/news-create.js
@@ -13,56 +13,108 @@ require.config({
     
     waitSeconds: 200,
     
-    paths: {
-        jquery     : "includes/jquery/jquery",
-        jpaginate  : "includes/jquery/jquery.paginate",
-        jqueryui   : "includes/jquery-ui/jquery-ui",
-        bootstrap  : "includes/bootstrap/bootstrap",
-        bootbox    : "includes/bootstrap/bootbox",
-        underscore : "includes/underscore/underscore",
-        backbone   : "includes/backbone/backbone",
-        tagsinput  : "includes/jquery/jquery.tagsinput",
-        redactor   : "includes/redactor/redactor",
-        dropzone   : "includes/dropzone/dropzone",
-        tour       : "includes/tour/bootstrap-tour",
-        leaflet    : "includes/leaflet/leaflet"
+    "paths": {
+        "jquery": "includes/jquery/jquery",
+        "jqueryui": "includes/jquery-ui/jquery-ui",
+        "jpaginate": "includes/jquery/jquery.paginate",
+        "bootstrap": "includes/bootstrap/bootstrap",
+        "bootbox": "includes/bootstrap/bootbox",
+        "underscore": "includes/underscore/underscore",
+        "backbone": "includes/backbone/backbone",
+        "paginator": "includes/backbone/backbone.paginator",
+        "moment": "includes/momentjs/moment",
+        "tagsinput": "includes/jquery/jquery.tagsinput",
+        "bootstrap-switch" : "includes/bootstrap/bootstrap-switch",
+        "redactor": "includes/redactor/redactor",
+        "dropzone": "includes/dropzone/dropzone",
+        "leaflet": "includes/leaflet/leaflet",
+        "fullpagejs": "includes/fullpagejs/jquery.fullPage",
+        "color": "includes/jquery/jquery.color",
+        "Jcrop": "includes/jquery/jquery.Jcrop",
+        "file-input": "includes/bootstrap/bootstrap.file-input",
+        "vector": "includes/vectormap/jquery-jvectormap-1.2.2.min",
+        "worldmap": "includes/vectormap/jquery-jvectormap-world-mill-en",
+        "plot": "includes/jqplot/jquery.jqplot",
+        "dateAxisRenderer": "includes/jqplot/plugins/jqplot.dateAxisRenderer",
+        "canvasTextRenderer": "includes/jqplot/plugins/jqplot.canvasTextRenderer",
+        "canvasAxisTickRenderer": "includes/jqplot/plugins/jqplot.canvasAxisTickRenderer",
+        "categoryAxisRenderer": "includes/jqplot/plugins/jqplot.categoryAxisRenderer",
+        "barRenderer": "includes/jqplot/plugins/jqplot.barRenderer",
+        "tubular": "includes/tubular/jquery.tubular.1.0",
+        "tour": "includes/tour/bootstrap-tour"
     },
-    
-    shim: {
+
+    "shim": {
         
-        jpaginate: {
-            deps: ["jquery"]
+        "jpaginate": {
+            "deps": ["jquery"]
         },
         
-        underscore: {
-            deps: ["jquery"],
-            exports: "_"
+        "underscore": {
+            "deps": ["jquery"],
+            "exports": "_"
         },
         
-        backbone: {
-            deps: ["underscore"],
-            exports: "Backbone"
+        "backbone": {
+            "deps": ["underscore"],
+            "exports": "Backbone"
         },
         
-        bootstrap: {
-            deps: ["jquery"]
+        "bootstrap": {
+            "deps": ["jquery"]
         },
         
-        bootbox: {
-            deps: ["bootstrap"]
+        "bootbox": {
+            "deps": ["bootstrap"],
+            "exports": "bootbox"
         },
         
-        jqueryui: {
-            deps: ["jquery"]
+        "tagsinput": {
+            "deps": ["jquery"]
+        },
+
+        "bootstrap-switch": {
+            "deps": ["bootstrap"]
+        },
+
+        "fullpagejs": {
+            "deps": ["jquery"]
         },
         
-        tagsinput: {
-            deps: ["jqueryui"]
-        }
+        "color": {
+            "deps": ["jquery"]
+        },
+        
+        "Jcrop": {
+            "deps": ["color"]
+        },
+        
+        "file-input": {
+            "deps": ["bootstrap"]
+        },
+        
+        "vector": {
+            "deps": ["jquery"]
+        },
+        
+        "worldmap": {
+            "deps": ["vector"]
+        },
+        
+        "plot": {
+            "deps": ["jquery"]
+        },
+        
+        "dateAxisRenderer": { "deps": ["plot"] },
+        "canvasTextRenderer": { "deps": ["plot"] },
+        "canvasAxisTickRenderer": { "deps": ["plot"] },
+        "categoryAxisRenderer": { "deps": ["plot"] },
+        "barRenderer": { "deps": ["plot"] }
     }
 });
 
 require(['jquery',
+         'jqueryui',
          'js/modules/common',
          'js/modules/editor/plugins/uploader',
          'js/modules/locations/follow',
