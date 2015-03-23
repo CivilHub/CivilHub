2e99b19bd969bc774c8da274c0fbfde622c12c0d
KarolMilewski <karol@civilhub.org>
2015-03-23 16:30:37 +0100

    merge karolnowy

--- articles/urls.py
+++ articles/urls.py
@@ -20,6 +20,5 @@ urlpatterns = patterns('',
     #url(r'^support/', views.SupportListView.as_view(), name='support'),
     url(r'^support/', views.TopLevelArticleView.as_view(article_slug='support', template_name='articles/support-list.html'), name='support'),
     url(r'^vector-map/', views.TopLevelArticleView.as_view(article_slug='vector-map', template_name='articles/vector-map.html'), name='vector-map'),
-    url(r'^do-testowania/', views.TopLevelArticleView.as_view(article_slug='do-testowania', template_name='articles/test.html'), name='do-testowania'),
     url(r'^brief/', views.TopLevelArticleView.as_view(article_slug='brief', template_name='articles/brief.html'), name='brief')
 )
