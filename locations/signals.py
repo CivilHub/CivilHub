f8ebe8f531f4dc9b1dd194a2df58fd6b0712f7d0
Jakub Pocentek <jpocentek@gmail.com>
2015-03-24 19:45:57 +0100

    Missing signals

--- locations/signals.py
+++ locations/signals.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 from django.core import cache
 from django.conf import settings
-from django.db.models import signals
 
 redis_cache = cache.get_cache('default')
 
+
 def update_parent_cache(sender, instance, created, **kwargs):
     """ Update cached lists of sublocations for parent location. """
-    if not created or not hasattr(instance, 'parent'):
+    if not created or instance.parent is None:
         return True
-    slug = instance.parent.slug
     for language in [x[0] for x in settings.LANGUAGES]:
         key = "{}_{}_sub".format(instance.parent.slug, language)
         redis_cache.set(key, instance.parent.location_set.all())
+
+
+def adjust_created_location(sender, instance, created, **kwargs):
+    """ Location creator will be moderator and follower. """
+    if created:
+        instance.creator.profile.mod_areas.add(instance)
+        instance.creator.profile.save()
