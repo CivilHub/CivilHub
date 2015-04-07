af9121920c1e1e218b40ff3040cc54299b04903c
KarolMilewski <karol@civilhub.org>
2015-04-07 16:28:09 +0200

    project features, btn antiman

--- articles/urls.py
+++ articles/urls.py
@@ -22,5 +22,6 @@ urlpatterns = patterns('',
     url(r'^support/', views.TopLevelArticleView.as_view(article_slug='support', template_name='articles/support-list.html'), name='support'),
     url(r'^vector-map/', views.TopLevelArticleView.as_view(article_slug='vector-map', template_name='articles/vector-map.html'), name='vector-map'),
     url(r'^brief/', views.TopLevelArticleView.as_view(article_slug='brief', template_name='articles/brief.html'), name='brief'),
-    url(r'^local-communities/', views.TopLevelArticleView.as_view(article_slug='local-communities', template_name='articles/brief.html'), name='brief')
+    url(r'^local-communities/', views.TopLevelArticleView.as_view(article_slug='local-communities', template_name='articles/brief.html'), name='brief'),
+    url(r'^project-features/', views.TopLevelArticleView.as_view(article_slug='project-features', template_name='articles/brief.html'), name='project-features')
 )
