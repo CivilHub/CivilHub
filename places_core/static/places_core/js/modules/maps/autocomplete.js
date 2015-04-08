9d3b167a285369bf7a133676de0d7ea0f0099e3a
Jakub Pocentek <jpocentek@gmail.com>
2015-04-08 18:36:08 +0200

    Minimap fixed

--- places_core/static/places_core/js/modules/maps/autocomplete.js
+++ places_core/static/places_core/js/modules/maps/autocomplete.js
@@ -1,76 +1,77 @@
 define(['jquery', 'underscore'], function ($, _) {
+  
+// Simplified autocomplete functionality to search for places - it allows
+// users to find and select single location to browse markers from.
+
+// TODO: setLocation method should be universal, the same applies to the template
+// TODO: timeout for 'keyup' event
+
+$.fn.autocomplete = function (options) {
+  
+  var defaults = {
+    // Clicking from a list of hints 
+    onSelect: function (data) {console.log(data);},
+    // Input field clear
+    onClear: function () {console.log("Input clear");}
+  };
     
-    // Simplified autocomplete functionality to search for places - it allows
-    // users to find and select single location to browse markers from.
+  return $(this).each(function () {
     
-    // TODO: setLocation method should be universal, the same applies to the template
-    // TODO: timeout for 'keyup' event
+    var $input = $(this),
+      opts = _.extend(defaults, options),
+      $ul = $('<ul class="custom-autocomplete"></ul>'),
+      tpl = ['<li><a href="#" data-location="{id}" data-lat="{lat}"',
+            ' data-lng="{lng}">{name}</a></li>'].join('');
     
-    $.fn.autocomplete = function (options) {
-        
-        var defaults = {
-            // Clicking from a list of hints 
-            onSelect: function (data) {console.log(data);},
-            // Input field clear
-            onClear: function () {console.log("Input clear");}
-        };
-            
-        return $(this).each(function () {
-            
-            var $input = $(this),
-                opts = _.extend(defaults, options),
-                $ul = $('<ul class="custom-autocomplete"></ul>'),
-                tpl = '<li><a href="#" data-location="{id}" data-lat="{lat}" \
-                       data-lng="{lng}">{name}</a></li>';
-            
-            function clearItems () {
-                $ul.find('li').empty().remove();
-            }
-            
-            function toggleList () {
-                if ($ul.find('li').length >= 1) {
-                    $ul.toggle();
-                }
-            }
-            
-            // FIXME: this should be a universal function
-            
-            function setLocation (data) {
-                clearItems();
-                $.each(data.results, function (idx, item) {
-                    var $li = $(tpl.replace(/{id}/g, item.id)
-                                   .replace(/{name}/g, item.name)
-                                   .replace(/{lat}/g, item.latitude)
-                                   .replace(/{lng}/g, item.longitude));
-                    $li.appendTo($ul);
-                    $li.find('a').on('click', function (e) {
-                        e.preventDefault();
-                        $input.val($(this).text());
-                        clearItems();
-                        // Fire callback onSelect()
-                        if (typeof(opts.onSelect === 'function')) {
-                            opts.onSelect($(this).data());
-                        }
-                    });
-                });
-            }
-                       
-            $ul.insertAfter($input);
-            
-            $input.on('keyup', function () {
-                
-                if ($input.val().length === 0) {
-                    // Fire callback onClear()
-                    if (typeof(opts.onClear) === 'function') {
-                        opts.onClear();
-                    }
-                    return false;
-                }
-                
-                $.get('/api-locations/markers/',{term:$input.val()},setLocation);
-            });
-            
-            $input.on('click', toggleList);
+    function clearItems () {
+      $ul.find('li').empty().remove();
+    }
+    
+    function toggleList () {
+      if ($ul.find('li').length >= 1) {
+        $ul.toggle();
+      }
+    }
+    
+    // FIXME: this should be a universal function
+    
+    function setLocation (data) {
+      clearItems();
+      $.each(data.results, function (idx, item) {
+        var $li = $(tpl.replace(/{id}/g, item.id)
+                 .replace(/{name}/g, item.name)
+                 .replace(/{lat}/g, item.latitude)
+                 .replace(/{lng}/g, item.longitude));
+        $li.appendTo($ul);
+        $li.find('a').on('click', function (e) {
+          e.preventDefault();
+          $input.val($(this).text());
+          clearItems();
+          // Fire callback onSelect()
+          if (typeof(opts.onSelect === 'function')) {
+            opts.onSelect($(this).data());
+          }
         });
-    };
+      });
+    }
+           
+    $ul.insertAfter($input);
+    
+    $input.on('keyup', function () {
+      
+      if ($input.val().length === 0) {
+        // Fire callback onClear()
+        if (typeof(opts.onClear) === 'function') {
+          opts.onClear();
+        }
+        return false;
+      }
+      
+      $.get('/api-locations/markers/',{term:$input.val()},setLocation);
+    });
+    
+    $input.on('click', toggleList);
+  });
+};
+
 });
--- places_core/static/places_core/js/modules/maps/pointer.js
+++ places_core/static/places_core/js/modules/maps/pointer.js
@@ -1,80 +1,123 @@
 //
 // pointer.js
 // ==========
-//
+
 // Script to load when you want to use map pointer form.
 
 require(['jquery',
+         'underscore',
          'js/modules/utils/utils',
          'js/modules/ui/ui',
+         'bootstrap',
          'js/modules/ui/mapinput'],
 
-function ($, utils, ui) {
+function ($, _, utils, ui) {
 
-  "use strict";
+"use strict";
 
-  $('.map-marker-toggle').bind('click', function (evt) {
+var BASE_URL = '/api-maps/objects/?ct={ct}&pk={id}';
+var ICON_URL = mapApp.staticUrl + '/css/images';
 
-    evt.preventDefault();
+function Minimap (el) {
+  this._state = false;
+  this.$toggle = $(el);
+  this.$modal = $('#minimap-modal');
+  this.$submit = this.$modal.find('[type="submit"]');
+  this.$input = this.$modal.find('#minimap');
+  this.ct = this.$toggle.attr('data-ct');
+  this.pk = this.$toggle.attr('data-id');
+  this.url = BASE_URL
+    .replace(/{ct}/g, this.ct)
+    .replace(/{id}/g, this.pk);
+}
 
-    var $modal   = $(_.template($('#map-marker-form-template').html(), {})),
-        $form    = $modal.find('form:first'),
-        $submit  = $modal.find('.submit-btn:first'),
-        formData = {};
+Minimap.prototype.initialize = function () {
+  this.$modal.on('hidden.bs.modal', function () {
+    this._state = false;
+  }.bind(this));
+  this.$modal.on('shown.bs.modal', function () {
+    this.setMapInput();
+  }.bind(this));
+  this.$submit.on('click', function (e) {
+    e.preventDefault();
+    this.sendMarkers();
+  }.bind(this));
+};
 
-    $modal.modal('show');
+// We need this method to run AFTER modal is opened, in other case
+// there are some issues with proper size and look of minimap.
 
-    $modal.on('shown.bs.modal', function () {
-
-      $('#id_latitude').before('<div class="mapinput"></div>');
-
-      $modal.find('.submit-btn').on('click', function (e) {
-        e.preventDefault();
-        $form.submit();
-      });
-
-      var markersUrl = ('/api-maps/objects/?ct={ct}&pk={pk}')
-        .replace(/{ct}/g, $('#id_content_type').val())
-        .replace(/{pk}/g, $('#id_object_pk').val());
-
-      $.get(markersUrl, function (markers) {
-        $('.mapinput').mapinput({
-          single: false,
-          width: 550,
-          height: 300,
-          markers: markers,
-          iconPath: ([window.STATIC_URL, 'css', 'images']).join('/')
-        });
-      });
-
-      $form.on('submit', function (e) {
-        e.preventDefault();
-        formData = {
-          csrfmiddlewaretoken: utils.getCookie('csrftoken'),
-          content_type: $('#id_content_type').val(),
-          object_pk: $('#id_object_pk').val(),
-          location: $('#current-location-pk').val(),
-          markers: JSON.stringify($.map($('.mapinput').data('mapinput').markers,
-            function (m) {
-              return {lat: m.getLatLng().lat, lng: m.getLatLng().lng};
-            }
-          ))
-        };
-        console.log(formData);
-        $.post('/api-maps/mapinput/', formData,
-          function (response) {
-            ui.message.success(gettext("Map pointer created"));
-          }
-        );
-
-        $modal.modal('hide');
-      });
-    });
-
-    $modal.on('hidden.bs.modal', function () {
-      $modal.empty().remove();
-      $('body').removeClass('modal-open');
-      $('.modal-backdrop').remove();
+Minimap.prototype.setMapInput = function () {
+  var $mapinput = this.$input;
+  if (!_.isUndefined($mapinput.data('mapinput'))) {
+    return true;
+  }
+  $.get(this.url, function (markers) {
+    $mapinput.mapinput({
+      single: false,
+      width: 550,
+      height: 300,
+      markers: markers,
+      iconPath: ICON_URL
     });
   });
+};
+
+Minimap.prototype.sendMarkers = function () {
+  var mapinput = this.$input.data('mapinput');
+  var markers = $.map(mapinput.markers, function (marker) {
+    return {lat: marker.getLatLng().lat, lng: marker.getLatLng().lng};
+  });
+  var data = {
+    csrfmiddlewaretoken: utils.getCookie('csrftoken'),
+    content_type: this.ct,
+    object_pk: this.pk,
+    markers: JSON.stringify(markers)
+  };
+  $.post('/api-maps/mapinput/', data, function (response) {
+    if (response.success) {
+      ui.message.success(response.message);
+    } else {
+      ui.message.danger(response.message);
+    }
+  });
+  // TODO: we assume that everything goes well.
+  // This is not good approach, but sufficient for now.
+  this._close();
+};
+
+Minimap.prototype._open = function () {
+  this.$modal.modal('show');
+  this._state = true;
+};
+
+Minimap.prototype._close = function () {
+  this.$modal.modal('hide');
+  this._state = false;
+};
+
+Minimap.prototype.toggle = function () {
+  if (this._state === true) {
+    this._close();
+  } else {
+    this._open();
+  }
+};
+
+$.fn.minimapToggler = function () {
+  return $(this).each(function () {
+    var map = new Minimap(this);
+    $(this).data('minimap', map);
+    $(this).on('click', function (e) {
+      e.preventDefault();
+      map.toggle();
+    });
+    map.initialize();
+  });
+};
+
+$(document).ready(function () {
+  $('.map-marker-toggle').minimapToggler();
+});
+
 });
