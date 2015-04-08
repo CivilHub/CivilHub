03f0bb29b1e48dbb44cef96cf7add81671890c58
Jakub Pocentek <jpocentek@gmail.com>
2015-04-08 11:17:40 +0200

    Clean up empty map markers

--- maps/tasks.py
+++ maps/tasks.py
@@ -8,6 +8,7 @@ from celery.task.base import periodic_task
 from locations.models import Location
 
 from .helpers import create_country_clusters, make_region_cluster
+from .models import MapPointer
 
 
 @periodic_task(run_every=datetime.timedelta(minutes=20))
@@ -16,3 +17,11 @@ def update_marker_cluster_cache():
     create_country_clusters()
     for location in Location.objects.filter(kind__in=['PPLA', 'PPLC',]):
         make_region_cluster(location)
+
+
+@periodic_task(run_every=datetime.timedelta(minutes=10))
+def clear_empty_markers():
+    """ There is a chance that deleted objects will stay on map. """
+    for marker in MapPointer.objects.all():
+        if marker.content_object is None:
+            marker.delete()
