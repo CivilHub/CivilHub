aa7c6a06ffe4979285e5119fa96177d5ec31526a
expose <expose@chcesieuczyc.pl>
2015-04-17 10:22:08 +0000

    test tpl, styles

--- articles/urls.py
+++ articles/urls.py
@@ -25,5 +25,6 @@ urlpatterns = patterns('',
     url(r'^local-communities/', views.TopLevelArticleView.as_view(article_slug='local-communities', template_name='articles/brief.html'), name='brief'),
     url(r'^project-features/', views.TopLevelArticleView.as_view(article_slug='project-features', template_name='articles/brief.html'), name='project-features'),
     url(r'^idea-to-law/', views.TopLevelArticleView.as_view(article_slug='idea-to-law', template_name='articles/brief.html'), name='idea-to-law'),
-    url(r'^idea-to-project/', views.TopLevelArticleView.as_view(article_slug='idea-to-project', template_name='articles/brief.html'), name='idea-to-project')
+    url(r'^idea-to-project/', views.TopLevelArticleView.as_view(article_slug='idea-to-project', template_name='articles/brief.html'), name='idea-to-project'),
+    url(r'^history-timeline/', views.TopLevelArticleView.as_view(article_slug='history-timeline', template_name='articles/test.html'), name='history-timeline'),
 )
