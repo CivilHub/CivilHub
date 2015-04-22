ad553452db55da83bb77688ac037b743a2322f65
expose <expose@chcesieuczyc.pl>
2015-04-22 11:31:03 +0000

    notifications

--- places_core/static/places_core/js/modules/notifications/view.js
+++ places_core/static/places_core/js/modules/notifications/view.js
@@ -5,25 +5,25 @@
 // View for single notification (single list item).
 
 define(['jquery',
-				'underscore',
-				'backbone',
-				'text!tpl/notify_entry.html'],
+        'underscore',
+        'backbone',
+        'text!js/modules/notifications/templates/notify_entry.html'],
 
 function ($, _, Backbone, html) {
 
 "use strict";
 
 var NotifyView = Backbone.View.extend({
-	template: _.template(html),
-	tagName: 'li',
-	className: "clearfix",
-	render: function () {
-		this.$el.html(this.template(this.model.toJSON()));
-		if (this.model.get('is_new')) {
-			this.$el.addClass('new');
-		}
-		return this;
-	}
+  template: _.template(html),
+  tagName: 'li',
+  className: "clearfix",
+  render: function () {
+    this.$el.html(this.template(this.model.toJSON()));
+    if (this.model.get('is_new')) {
+      this.$el.addClass('new');
+    }
+    return this;
+  }
 });
 
 return NotifyView;
