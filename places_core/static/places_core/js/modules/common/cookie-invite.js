9b8c0f2bbec2ca631912c5ba8d030d54ef7ba220
expose <expose@chcesieuczyc.pl>
2015-04-30 11:27:14 +0000

    Add and fix cookie warning

---
+++ places_core/static/places_core/js/modules/common/cookie-invite.js
@@ -0,0 +1,40 @@
+//
+// cookie-invite.js
+// =================
+
+// Displays a cookie invite friends.
+
+require(['jquery',
+         'underscore',
+         'js/modules/utils/utils',
+         'text!tpl/cookie_if.html'],
+
+function ($, _, utils, html) {
+
+"use strict";
+
+function showCookieInvite () {
+  var nav = $("#navbar-top");
+
+  if (utils.getCookie("cookie-if") && CivilApp.debug) {
+    return;
+  }
+
+  console.log(html);
+  $('#cookie-if').prepend(_.template(html), {})
+    .hide().fadeIn('fast');
+  $(nav).addClass('nav-cookie');
+  $('body').addClass('body-cookie');
+  $('.accept-btn-if').click(function () {
+    $(nav).removeClass('nav-cookie');
+    $('body').removeClass('body-cookie');
+    utils.setCookie('cookie-if', true, 14);
+    $('#cookie-if').fadeOut('fast', function () {
+      $(this).empty().remove();
+    });
+  });
+}
+
+$(document).ready(showCookieInvite);
+
+});
