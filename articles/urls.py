b613786390c4a6b3a51794157a9a25f45f5d0e11
bartek <bartek@expose.pl>
2015-05-04 16:46:41 +0200

    Adds meetup-szczecin and hackatlon-warsaw to urls.py

--- articles/urls.py
+++ articles/urls.py
@@ -33,4 +33,6 @@ urlpatterns = patterns('',
     url(r'^open-source/', views.TopLevelArticleView.as_view(article_slug='open-source', template_name='articles/clear.html'), name='open-source'),
     url(r'^licence/', views.TopLevelArticleView.as_view(article_slug='licence', template_name='articles/licence.html'), name='licence'),
     url(r'^events/', views.TopLevelArticleView.as_view(article_slug='events', template_name='articles/events.html'), name='events'),
+    url(r'^meetup-szczecin-1/', views.TopLevelArticleView.as_view(article_slug='meetup-szczecin-1', template_name='articles/events.html'), name='meetup-szczecin-1'),
+    url(r'^hackatlon-warszawa-1/', views.TopLevelArticleView.as_view(article_slug='hackatlon-warszawa-1', template_name='articles/events.html'), name='hackatlon-warszawa-1'), 
 )
