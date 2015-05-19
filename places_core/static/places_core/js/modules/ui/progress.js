f23f4e129ffdbbbfff32a919208b67ca287b0849
Jakub Pocentek <jpocentek@gmail.com>
2015-05-19 18:04:48 +0200

    New scripts compressed

--- places_core/static/places_core/js/modules/ui/progress.js
+++ places_core/static/places_core/js/modules/ui/progress.js
@@ -12,7 +12,8 @@ function ($, _) {
 
 "use strict";
 
-var template = '<div class="container-fluid"><div class="progress"><div class="progress-bar progress-bar-striped"' +
+var template = '<div class="container-fluid"><div class="progress">' +
+               '<div class="progress-bar progress-bar-striped"' +
                ' role="progressbar" aria-valuenow="0" aria-valuemin="0"' +
                ' aria-valuemax="100" style="width: 0%;">0%</div></div></div>';
 
