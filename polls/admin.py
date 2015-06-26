ac28caf529cc72ebb65e7a350273f2914cc26cc0
Jakub Pocentek <jpocentek@gmail.com>
2015-06-26 16:14:46 +0200

    Admin panel for polls

--- polls/admin.py
+++ polls/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
 from ordered_model.admin import OrderedModelAdmin
 
@@ -11,23 +12,17 @@ admin.site.register(Poll)
 
 
 class SimplePollAdmin(admin.ModelAdmin):
-    """
-    """
     readonly_fields = ('slug', )
 
 admin.site.register(SimplePoll, SimplePollAdmin)
 
 
 class SimplePollInlineAnswerAdmin(admin.TabularInline):
-    """
-    """
     model = SimplePollAnswer
     list_display = ('text', 'move_up_down_links', )
 
 
 class SimplePollQuestionAdmin(OrderedModelAdmin):
-    """
-    """
     list_display = ('text', 'question_type', 'move_up_down_links', )
     inlines = (SimplePollInlineAnswerAdmin, )
 
@@ -35,8 +30,20 @@ admin.site.register(SimplePollQuestion, SimplePollQuestionAdmin)
 
 
 class SimplePollAnswerSetAdmin(admin.ModelAdmin):
-    """
-    """
-    list_display = ('user', 'poll', 'question', )
+    list_filter = ('poll', )
+    list_display = ('user_full_name', 'poll', 'question', 'get_answers')
+    search_fields = ('user__email', 'user__first_name', 'user__last_name', )
+
+    def get_answers(self, obj):
+        if obj.question.question_type == 3:
+            return obj.answer
+        answer_ids = [int(x.strip()) for x in obj.answer.split(',')]
+        answers = SimplePollAnswer.objects.filter(pk__in=answer_ids)
+        return ", ".join([x.__unicode__() for x in answers])
+    get_answers.short_description = _(u"answers")
+
+    def user_full_name(self, obj):
+        return obj.user.get_full_name()
+    user_full_name.short_description = _(u"user")
 
 admin.site.register(SimplePollAnswerSet, SimplePollAnswerSetAdmin)
