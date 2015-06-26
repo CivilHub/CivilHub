d5e9f3a31ce7c3c8e7236ad4446113facca9ddaa
Jakub Pocentek <jpocentek@gmail.com>
2015-06-26 17:39:23 +0200

    Admin changes - first part

--- polls/admin.py
+++ polls/admin.py
@@ -8,7 +8,18 @@ from .models import Poll, SimplePoll, SimplePollQuestion, \
 
 
 admin.site.register(SimplePollAnswer)
-admin.site.register(Poll)
+
+
+class PollAdmin(admin.ModelAdmin):
+    search_fields = ('question', )
+    list_filter = ('date_created', )
+    list_display = ('__unicode__', 'get_username', 'location', 'date_created', )
+
+    def get_username(self, obj):
+        return obj.creator.get_full_name()
+    get_username.short_description = _(u"user")
+
+admin.site.register(Poll, PollAdmin)
 
 
 class SimplePollAdmin(admin.ModelAdmin):
--- topics/admin.py
+++ topics/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 from .models import Category, Discussion, Entry
 
 admin.site.register(Category)
-admin.site.register(Discussion)
 admin.site.register(Entry)
+
+
+class DiscussionAdmin(admin.ModelAdmin):
+    search_fields = ('question', )
+    list_filter = ('date_created', )
+    list_display = ('__unicode__', 'get_username', 'location', 'date_created', )
+
+    def get_username(self, obj):
+        return obj.creator.get_full_name()
+    get_username.short_description = _(u"user")
+
+admin.site.register(Discussion, DiscussionAdmin)
