e6573e188bb48db93afd2a25e3983fec2067c000
Jakub Pocentek <jpocentek@gmail.com>
2015-06-29 11:14:05 +0200

    Scrapper for sitemap

---
+++ utils/scrapper.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import urllib2
+from bs4 import BeautifulSoup as BS
+
+from django.contrib.sites.models import Site
+
+
+def parse_sitemap(url):
+    f = urllib2.urlopen(url)
+    data = BS(f.read())
+    links = [x.text for x in data.body.find_all('loc')]
+    return links
+
+
+def find_location(slug):
+    url = 'http://{}/sitemap.xml'.format(Site.objects.get_current().domain)
+    links = [x for x in parse_sitemap(url) if 'sitemap-locations' in x]
+    locations = []
+    for link in links:
+        locations += [x for x in parse_sitemap(link) if slug in x]
+    return locations
