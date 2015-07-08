29e980a4482732db26a48e7e22d7a26347ad87aa
KarolMilewski <karol@civilhub.org>
2015-07-08 15:23:16 +0200

    Merge - master

--- places_core/static/places_core/js/modules/ui/active-area.js
+++ places_core/static/places_core/js/modules/ui/active-area.js
@@ -2,7 +2,17 @@
 // active-area.js
 // ==============
 
-// Press @ to get userlist.
+// Press @ to get userlist. Scripts that handles simple macro. We can connect
+// with server and fetch list of users that first or last name matches text
+// in input box (textarea as well as text inputs are supported).
+//
+// The only mandatory option is the `el` option as in regular Backbone views.
+// Only this way we can bind plugin with DOM element. So the simples use case
+// will be something like:
+//
+// var aa = new ActiveArea({ el: '#my-text-area' });
+//
+// And that's all! Plugin itself doesn't provide any 'default' template.
 
 define(['jquery',
         'underscore',
@@ -18,11 +28,17 @@ var ResultCollection = Backbone.Collection.extend({
   url: '/api-userspace/mention/'
 });
 
+// Single result entry with profile picture and user ful name.
+
 var ResultView = Backbone.View.extend({
 
   tagName: 'li',
 
   template: _.template(resultHTML),
+
+  // Mark filtered out entries as 'hidden'.
+
+  _hidden: false,
 
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
@@ -33,20 +49,44 @@ var ResultView = Backbone.View.extend({
     return this;
   },
 
+  // Show entry if it was hidden and match search phrase.
+
   show: function () {
     this.$el.show();
+    this._hidden = false;
   },
+
+  // Hide entry if user full name doesn't match search phrase.
 
   hide: function () {
     this.$el.hide();
+    this._hidden = true;
   }
 });
 
+// Main object that holds entire area bind to single input
+// entry (usually this will be textarea element).
+
 var ActiveArea = Backbone.View.extend({
+
+  // Marks plugin as active - searching etc. is enabled if true
 
   _active: false,
 
+  // Marks plugin as initialized - usually we fetch
+  // initial data only once and set this flag to true.
+
   _initialized: false,
+
+  // Holds view elements bound with collection models via
+  // ID attribute. Model's ID becomes key in this object.
+
+  _items: {},
+
+  // Marks plugin as 'waiting' when there is no more results to show.
+  // User may either write regular text or press backspace to see list again.
+
+  _waiting: false,
 
   initialize: function (options) {
     this.$el.parent().addClass('active-area-placeholder');
@@ -61,6 +101,8 @@ var ActiveArea = Backbone.View.extend({
     this.replaceContent();
   },
 
+  // Replace initial input content with proper macros representaions.
+  //
   // FIXME: find some more efficient way to replace mention links.
 
   replaceContent: function () {
@@ -75,6 +117,8 @@ var ActiveArea = Backbone.View.extend({
     this.$el.val(value);
   },
 
+  // Check if input is already active and process it or activate plugin.
+
   checkInput: function (keyCode) {
     if (this._active) {
       this.checkActiveCode(keyCode);
@@ -85,20 +129,46 @@ var ActiveArea = Backbone.View.extend({
     }
   },
 
+  // This is core function that handles most of program logic.
+  //
+  // When plugin is active, this function checks, what key was pressed by user
+  // and decide what to do next (either filter result list or deactivate plugin).
+
   checkActiveCode: function (keyCode) {
-    if (keyCode === 32) {
-      this._active = false;
-      this._initialized = false;
-      this.$list.fadeOut('slow');
+
+    // Special case of 'waiting' state. Here user can resign or press
+    // backspace key to see result list again and keep it working.
+
+    if (this._waiting) {
+      switch (keyCode) {
+        case 32:
+          this._active = false;
+          this._initialized = false;
+          break;
+        case 8:
+          this.$list.fadeIn('fast');
+          break;
+      }
+      this._waiting = false;
       return;
     }
+
+    // If there is nothing to process, we obviously has nothing to do.
+
     var term = this.getActiveTxt();
     if (!term) {
       return;
     }
+
+    // Fetch list again if user will back to 3 or less characters again.
+
     if (keyCode === 8 && term.length <= 3) {
       this._initialized = false;
     }
+
+    // Check if search term is reasonably long and either fetch new results
+    // from server or filter existing entries to match search query.
+
     if (term.length >= 3) {
       if (!this._initialized) {
         this.collection.fetch({
@@ -111,17 +181,32 @@ var ActiveArea = Backbone.View.extend({
     }
   },
 
+  // Narrow list to display only fields containing users that match search term.
+
   filter: function (s) {
     var re = new RegExp(s, 'i');
     this.collection.each(function (model) {
-      var view = $('li[data-resultId="' + model.get('id') + '"]');
+      var view = this._items[model.get('id')];
       if (!re.test(model.get('label'))) {
         view.hide();
       } else {
         view.show();
       }
     }, this);
+
+    // Find out if suggestion list is already empty and hide plugin if so.
+
+    var chk = _.every(this._items, function (item) {
+      return item._hidden;
+    });
+    if (_.isEmpty(this._items)) chk = false;
+    if (chk && this.getActiveTxt().length > 3) {
+      this._waiting = true;
+      this.$list.fadeOut('slow');
+    }
   },
+
+  // Returns currently processed macro text.
 
   getActiveTxt: function () {
     var value = this.$el.val();
@@ -130,13 +215,21 @@ var ActiveArea = Backbone.View.extend({
     return value.slice(start, stop);
   },
 
+  // Helper that returns caret position inside input element.
+  //
+  // This is required for proper display position for result list.
+
   getCaretPosition: function () {
     return this.$el[0].selectionStart;
   },
 
+  // Last marker that opened macro and initialized plugin.
+
   currentMarkerPosition: function () {
     return this.$el.val().lastIndexOf('@') + 1;
   },
+
+  // Presents entire results list. Usually used only once on initial fetch.
 
   displayResults: function () {
     this.$list.empty().hide();
@@ -152,11 +245,22 @@ var ActiveArea = Backbone.View.extend({
     this.$list.fadeIn('slow');
   },
 
+  // Create single result view object and append it to DOM.
+
   displayResult: function (item) {
     var result = new ResultView({ model: item });
     $(result.render().el).appendTo(this.$list);
     this.listenTo(result, 'activate', this.triggerCallback);
+
+    // Bind view and model in inner plugin registry.
+
+    this._items[item.id] = result;
   },
+
+  // Final function when user selected some position.
+  //
+  // Creates proper macro based on selected entry and
+  // replaces input value accordingly.
 
   triggerCallback: function (model) {
     var start = this.currentMarkerPosition() - 1;
